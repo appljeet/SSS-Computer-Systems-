c746c65a740eca82c48d7421ddf5b00692822640
appljeet <38896776+appljeet@users.noreply.github.com>
2021-11-05 17:52:52 -0700

    Update realtimeclock.py

--- Main Code/realtimeclock.py
+++ Main Code/realtimeclock.py
@@ -1,1 +1,12 @@
+# sudo pip3 install adafruit-circuitpython-ds3231
+# set RTC manually so that we don't reset the time when sat. restarts: sudo hwclock --set --date="Aug-22-2016 18:00:00"
+import adafruit_ds3231
+import time
+import board
 
+i2c = board.I2C()  # uses board.SCL and board.SDA
+rtc = adafruit_ds3231.DS3231(i2c)
+
+t = rtc.datetime
+print(t)
+print(t.tm_hour, t.tm_min)
