1a173b94f37d264ecf386121185cab6e67f1c582
UchihaCryptic <71690828+UchihaCryptic@users.noreply.github.com>
2022-03-06 18:21:01 -0800

    Specified resolution for camera to take photos

--- mainCode/piCamera.py
+++ mainCode/piCamera.py
@@ -6,6 +6,7 @@ from time import sleep
 camera = PiCamera()
 
 def capture():
+    camera.resolution(1920, 1080)
     timestamp = datetime.now().isoformat()
     sleep(3)
     camera.capture('../images/%s.jpg' % timestamp)
