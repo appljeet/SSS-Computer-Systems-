44f651263dc601518b35f18deecde67024e55aac
appljeet <38896776+appljeet@users.noreply.github.com>
2022-05-14 16:26:47 -0700

    Update checkBattery.py

--- mainCode/checkBattery.py
+++ mainCode/checkBattery.py
@@ -3,6 +3,6 @@ from rechargeMode import *
 
 def checkBattery():
     batteryPercentage = None; #set this variable to whatever the percentage should be
-    if batteryPercentage>60:
+    if batteryPercentage<60:
         #go into recharge loop otherwise this will go back to main
         rechargeMode(batteryPercentage)
